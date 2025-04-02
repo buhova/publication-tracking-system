5a107a89f84da67e9aab9289d71c54e17a21ddca
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-04-02 17:35:45 +0200

    feat(models): add Newspaper model
    This model will be used to create newspapers

--- publication/models.py
+++ publication/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import ManyToManyField
-
 from django.conf import settings
 
 
@@ -24,3 +22,19 @@ class Redactor(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic, on_delete=models.SET_NULL, null=True, blank=True, related_name="newspapers"
+    )
+    redactor = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
