1d657101a056e8281b0b442bf16a3041eb9191d4
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-04-02 18:20:02 +0200

    feat(admin): register Newspaper model in admin

--- publication/admin.py
+++ publication/admin.py
@@ -13,3 +13,10 @@ class TopicAdmin(admin.ModelAdmin):
 class RedactorAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "last_name", "email", "years_of_experience")
     search_fields = ("first_name", "last_name",)
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "topic", "published_date",)
+    search_fields = ("title",)
+    list_filter = ("topic",)
