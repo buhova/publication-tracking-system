53e434cc75090f47b07c64cd8d57f8cd47ecff42
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-04-02 19:33:19 +0200

    feat(templates): create basic HTML files with temporary layout

--- publication/views.py
+++ publication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from publication.models import Topic, Redactor, Newspaper
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    context = {
+        "num_topics": Topic.objects.count(),
+        "num_newspapers": Newspaper.objects.count(),
+        "num_redactors": Redactor.objects.count(),
+    }
+
+    return render(request, "publication/index.html", context=context)
+
