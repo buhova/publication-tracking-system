75d828aca574fb57532d2f6ae0aba8bf3478d793
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-04-02 19:58:13 +0200

    feat(views, urls): add list and detail views for models, create empty templates for them

--- publication/urls.py
+++ publication/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-
-from .views import (index,)
+from .views import (
+    index,
+    TopicListView,
+    RedactorListView,
+    RedactorDetailView,
+    NewspaperListView,
+    NewspaperDetailView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactor/<int:pk>", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("newspapers/<int:pk>", NewspaperDetailView.as_view(), name="newspaper-detail"),
 ]
 
 app_name = "publication"
--- publication/views.py
+++ publication/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
 from publication.models import Topic, Redactor, Newspaper
 
@@ -14,3 +16,38 @@ def index(request):
 
     return render(request, "publication/index.html", context=context)
 
+
+# class ManufacturerListView(LoginRequiredMixin, generic.ListView):
+#     model = Manufacturer
+#     context_object_name = "manufacturer_list"
+#     template_name = "taxi/manufacturer_list.html"
+#     paginate_by = 5
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "publication/topic_list.html"
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    context_object_name = "redactor_list"
+    template_name = "publication/redactor_list.html"
+
+
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Redactor
+    context_object_name = "redactor_detail"
+    template_name = "publication/redactor_detail.html"
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    context_object_name = "newspaper_list"
+    template_name = "publication/newspaper_list.html"
+
+
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Redactor
+    context_object_name = "newspaper_detail"
+    template_name = "publication/newspaper_detail.html"
