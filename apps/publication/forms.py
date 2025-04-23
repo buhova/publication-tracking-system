1bd50e7a65abb3ae863d6545a198f6554840a13d
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-04-23 22:58:11 +0200

    feat(forms): add forms for topic creation and redactor name search

--- apps/publication/forms.py
+++ apps/publication/forms.py
@@ -13,3 +13,19 @@ class NewspaperForm(forms.ModelForm):
     class Meta:
         model = Newspaper
         fields = ["title", "content", "topic", "redactor"]
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = "__all__"
+
+
+# Search forms
+class RedactorNameSearchForm(forms.Form):
+    query = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search"}),
+    )
