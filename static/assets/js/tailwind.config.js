04e67d7746f8ea6fea251e2dcbaf87277370049d
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-05-15 22:16:29 +0200

    chore(static): add third-party assets (fonts, JS, CSS) for UI

---
+++ static/assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/assets/js/**/*.js',
+  ],
+  darkMode: false, // або 'media', або 'class' (якщо потрібно)
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
