bb6b05b0cf5832ad7132d1b3cfc013e2b3784bba
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-06-02 18:17:05 +0200

    chore(settings): add WhiteNoise and update STATIC_ROOT for static files

--- publication_tracking_system/settings/base.py
+++ publication_tracking_system/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -114,7 +115,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 
 # Assets Management
