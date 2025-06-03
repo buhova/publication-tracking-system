b1135c64550362bc1afadaaca82ecd72c2b80eff
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-06-03 14:00:39 +0200

    chore(settings): add RENDER_EXTERNAL_HOSTNAME and SECRET_KEY to prod configuration

--- publication_tracking_system/settings/prod.py
+++ publication_tracking_system/settings/prod.py
@@ -8,7 +8,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
